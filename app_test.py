893f87c3970d47e9238df81b9345f5594b4748a6
MuriloMoreir <124629928+MuriloMoreir@users.noreply.github.com>
2024-09-06 08:04:49 -0300

    Create app_test.py

---
+++ app_test.py
@@ -0,0 +1,20 @@
+import unittest
+from app import app
+
+class FlaskAppTests(unittest.TestCase):
+  
+ def setUp(self):
+   self.client = app.test_client()
+
+ def test_devops(self):
+   response = self.client.get('/')
+   self.assertEqual(response.status_code, 200)
+   self.assertIn(b'Germinare',response.data)
+
+ def test_murilo(self):
+   response = self.client.get('/Murilo')
+   self.assertEqual(response.status_code, 200)
+   self.assertIn(b'MURILO',response.data)
+   
+if __name__ == '__main__':
+ unittest.main()
